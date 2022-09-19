8bbe8dfbd20749564995661fd3c0b3e598fc8ab9
Fahad Imran <fahad.imran@devsinc.com>
2022-09-20 00:47:36 +0500

    feat: configure firebase with app

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -4,7 +4,7 @@ import 'package:stacked_architecture/ui/startup/startup_view.dart';
 import 'package:stacked_services/stacked_services.dart';
 
 @StackedApp(routes: [
-  MaterialRoute(page: StartupView, initial: true),
+  CupertinoRoute(page: StartupView, initial: true),
   CupertinoRoute(page: SecondView),
 ], dependencies: [
   LazySingleton(classType: NavigationService)
